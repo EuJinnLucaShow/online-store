59212ca6025ada13ac65e901388591baabc9aabf
Yevhenii Lukashov <115802889+EuJinnLucaShow@users.noreply.github.com>
2023-12-20 16:24:33 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  routes: [{ "src": "/[^.]+", "dest": "/", "status": 200 }] 
+  rewrites: [
+    {
+      "source": "/(.*)",
+      "destination": "/"
+    }
+  ]
 })
