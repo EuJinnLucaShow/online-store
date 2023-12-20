6b78cb1df8ab14399b55a075559595f785dadf50
Yevhenii Lukashov <115802889+EuJinnLucaShow@users.noreply.github.com>
2023-12-20 16:25:40 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,18 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  rewrites: [
-    {
-      "source": "/(.*)",
-      "destination": "/"
-    }
-  ]
+  server: {
+    middleware: [
+      (req, res, next) => {
+        // Redirect all routes to '/'
+        if (req.url !== '/') {
+          res.writeHead(301, { Location: '/' })
+          res.end()
+        } else {
+          next()
+        }
+      }
+    ]
+  }
 })
+
