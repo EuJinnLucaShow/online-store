1151ac87f095173efe68d88ad1173e056a55edfa
Yevhenii Lukashov <115802889+EuJinnLucaShow@users.noreply.github.com>
2023-12-20 13:01:06 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/">
       <App />
       </BrowserRouter>
   </React.StrictMode>,
