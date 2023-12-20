2ab7a7450fc5436a8fa9bb399e0bf750a7f1b8bf
Yevhenii Lukashov <115802889+EuJinnLucaShow@users.noreply.github.com>
2023-12-20 22:05:40 +0200

    Summary

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,32 +1,42 @@
-import { NavLink } from 'react-router-dom';
-import icon from '../../assets/icon.svg'
-import './index.css'
+import { NavLink } from "react-router-dom";
+import icon from "../../assets/icon.svg";
+import "./index.css";
 
 export const Header = () => {
-    return (
-        <header className='header'>
-            <div className="header__container">
-                <div className="header__item">
-                    <div className="header__logo">
-                        <svg ><use xlinkHref={`${icon}#icon-logo`}></use></svg>
-                         <NavLink to="/">
-                            <span>Sound Line</span>
-                        </NavLink>
-                    </div>
-                    <NavLink to="/catalog" className="header__catalog">Каталог</NavLink>
-                </div>
-                <div className="header__input">
-                    <input type="text" placeholder='Що ви шукаєте?'/>
-             <svg ><use xlinkHref={`${icon}#icon-search`}></use></svg>
-                    <button>Пошук</button>
-                </div>
-                <div className="header__curt">
-<svg ><use xlinkHref={`${icon}#icon-shopping-cart`}></use></svg>
-                </div>
-                <div className='header__call'>
-<svg ><use xlinkHref={`${icon}#icon-phone`}></use></svg>
-                </div>
-            </div>
-        </header>
-    )
-}
+  return (
+    <header className="header">
+      <div className="header__container">
+        <div className="header__item">
+          <div className="header__logo">
+            <svg>
+              <use xlinkHref={`${icon}#icon-logo`}></use>
+            </svg>
+            <NavLink to="/">
+              <span>Sound Line</span>
+            </NavLink>
+          </div>
+          <NavLink to="/catalog" className="header__catalog">
+            Каталог
+          </NavLink>
+        </div>
+        <div className="header__input">
+          <input type="text" placeholder="Що ви шукаєте?" />
+          <svg>
+            <use xlinkHref={`${icon}#icon-search`}></use>
+          </svg>
+          <button>Пошук</button>
+        </div>
+        <div className="header__curt">
+          <svg>
+            <use xlinkHref={`${icon}#icon-shopping-cart`}></use>
+          </svg>
+        </div>
+        <div className="header__call">
+          <svg>
+            <use xlinkHref={`${icon}#icon-phone`}></use>
+          </svg>
+        </div>
+      </div>
+    </header>
+  );
+};
