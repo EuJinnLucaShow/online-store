abb511de2a3b50d8554ea5cd6b517dbfcd7a7127
Yevhenii Lukashov <115802889+EuJinnLucaShow@users.noreply.github.com>
2023-12-20 16:20:45 +0200

    Summary

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 export const Footer = () => {
     return (
         <footer className='footer'>
@@ -8,7 +6,7 @@ export const Footer = () => {
                 <div className="header__logo">
                         <svg width="69" height="73" viewBox="0 0 69 73" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <g id="Logo">
-                                <path id="logo" d="M3 3V70M3 3L66 70M3 3H66M3 70L66 3M3 70H66M66 3V70" stroke="#6B6666" stroke-width="5"/>
+                                <path id="logo" d="M3 3V70M3 3L66 70M3 3H66M3 70L66 3M3 70H66M66 3V70" stroke="#6B6666" strokeWidth="5"/>
                             </g>
                         </svg>
                         <span>Звукоряд</span>
