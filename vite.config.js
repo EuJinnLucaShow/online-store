b1c103805588e022d3b38b75629952360c8faab1
Yevhenii Lukashov <115802889+EuJinnLucaShow@users.noreply.github.com>
2023-12-21 23:13:18 +0200

    Summary

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    svgr({
-      // svgr options: https://react-svgr.com/docs/options/
-      svgrOptions: {
-        // ...
-      },
-
-      // esbuild options, to transform jsx to js
-      esbuildOptions: {
-        // ...
-      },
-
-      // A minimatch pattern, or array of patterns, which specifies the files in the build the plugin should include.
-      include: '**/*.svg?react',
-
-      //  A minimatch pattern, or array of patterns, which specifies the files in the build the plugin should ignore. By default no files are ignored.
-      exclude: '',
-    }),
-  ],
+  plugins: [react()],
 });
