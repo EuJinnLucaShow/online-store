33918e301d275da7d5f327f14aee41140b30f7cd
ArtemMerkushyn <merkushyndev13@gmail.com>
2023-12-23 19:47:22 +0200

    new commit

---
+++ src/components/Button/Button.styled.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Btn = styled.button`
+    padding: 5px 20px;
+    border-radius: 6px;
+    color: #EBDFDF;
+    font-size: 24px;
+    font-weight: 400;
+    cursor: pointer;
+    border: none;
+    display: block;
+`
