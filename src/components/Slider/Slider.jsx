3f6b333ff29571aa7c51cd40e266b61f7197e660
Yevhenii Lukashov <115802889+EuJinnLucaShow@users.noreply.github.com>
2023-12-25 23:52:56 +0200

    Summary

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,17 +1,17 @@
-import { SliderWrapper, StyledSlider } from "./Slider.styled";
-import image from '../../assets/slider/image1.png'
+import { SliderWrapper, StyledSlider } from './Slider.styled';
+import image from '../../assets/image.png';
 
 export const MySlider = () => {
-    return (
-        <SliderWrapper>
-            <StyledSlider>
-                <div className='slider__item'>
-                    <img src={image} alt='img' />
-                </div>
-                <div className='slider__item'>
-                    <img src={image} alt='img' />
-                </div>
-            </StyledSlider>
-        </SliderWrapper>
-    );
-}
+  return (
+    <SliderWrapper>
+      <StyledSlider>
+        <div className="slider__item">
+          <img src={image} alt="img" />
+        </div>
+        <div className="slider__item">
+          <img src={image} alt="img" />
+        </div>
+      </StyledSlider>
+    </SliderWrapper>
+  );
+};
